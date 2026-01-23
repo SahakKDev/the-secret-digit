97a7f79c2ab810fa40319a1d4180c2e5d68d0159
SahakKDev <sahak.keshishyan.dev@gmail.com>
2026-01-23 17:30:06 +0400

    fix highscore logic, reuse wrong secret number chose logic, update bg and box width when user wins

--- main.js
+++ main.js
@@ -1,28 +1,40 @@
-"use strict";
+'use strict';
 
 let SECRET_NUMBER = null;
+let HIGH_SCORE = 0;
 let SCORE = 20;
 
-const secretBox = document.querySelector(".secret-box");
-const secretNumberInput = document.querySelector(".secret-number-input");
-const infoLabel = document.querySelector(".info-label");
-const scoreLabel = document.querySelector(".score");
-const highScoreLabel = document.querySelector(".high-score");
+const secretBox = document.querySelector('.secret-box');
+const secretNumberInput = document.querySelector('.secret-number-input');
+const infoLabel = document.querySelector('.info-label');
+const scoreLabel = document.querySelector('.score');
+const highScoreLabel = document.querySelector('.high-score');
 
-const checkButton = document.querySelector(".check-button");
-const restartButton = document.querySelector(".restart");
+const checkButton = document.querySelector('.check-button');
+const restartButton = document.querySelector('.restart');
 
-checkButton.addEventListener("click", function () {
+checkButton.addEventListener('click', function () {
   const selectedNumber = +secretNumberInput.value;
 
   if (SECRET_NUMBER === selectedNumber) {
     // WIN GAME
-    infoLabel.textContent = "🎉 You Won!";
+    displayMessage('🎉 You Won!');
     secretBox.textContent = SECRET_NUMBER;
     secretNumberInput.disabled = true;
     checkButton.disabled = true;
-    highScoreLabel.textContent = SCORE;
+    document.body.style.backgroundColor = '#548325';
+    secretBox.style.width = '14rem';
 
+    if (HIGH_SCORE < SCORE) {
+      highScoreLabel.textContent = SCORE;
+      HIGH_SCORE = SCORE;
+    }
+
+    return;
+  }
+
+  if (!selectedNumber) {
+    displayMessage('⛔ No Number!');
     return;
   }
 
@@ -30,35 +42,40 @@ checkButton.addEventListener("click", function () {
 
   if (SCORE === 0) {
     // LOSE GAME
-    infoLabel.textContent = "😢 You Lose! Try Again...";
+    displayMessage('💣 You Lose!');
     secretBox.textContent = SECRET_NUMBER;
     secretNumberInput.disabled = true;
     checkButton.disabled = true;
+    scoreLabel.textContent = SCORE;
     return;
   }
 
   scoreLabel.textContent = SCORE;
 
-  if (selectedNumber > SECRET_NUMBER) {
-    infoLabel.textContent = "📈 Too High";
-  }
-
-  if (selectedNumber < SECRET_NUMBER) {
-    infoLabel.textContent = "📉 Too Low";
+  if (selectedNumber !== SECRET_NUMBER) {
+    displayMessage(
+      selectedNumber > SECRET_NUMBER ? '📈 Too High' : '📉 Too Low',
+    );
   }
 });
 
-restartButton.addEventListener("click", startNewGame);
+restartButton.addEventListener('click', startNewGame);
 
 function startNewGame() {
   SECRET_NUMBER = Math.trunc(Math.random() * 20) + 1;
-  secretBox.textContent = "?";
-  infoLabel.textContent = "Start guessing...";
+  secretBox.textContent = '?';
+  infoLabel.textContent = 'Start guessing...';
   secretNumberInput.disabled = false;
   checkButton.disabled = false;
-  secretNumberInput.value = "";
+  secretNumberInput.value = '';
   SCORE = 20;
   scoreLabel.textContent = SCORE;
+  document.body.style.backgroundColor = '#3e3e3e';
+  secretBox.style.width = '7rem';
 }
 
 startNewGame();
+
+function displayMessage(message) {
+  document.querySelector('.info-label').textContent = message;
+}
